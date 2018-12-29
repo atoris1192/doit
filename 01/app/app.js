30d9659ddba71a777944853fea0ce586939f2ea1
atoris1192 <atoris1192@gmail.com>
2018-12-29 15:40:57 +0900

    bruch artcle user

--- 01/app/app.js
+++ 01/app/app.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const router = require('./routes/v1/')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use('/api/v1/', router)
 
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Server start')
 })
-app.get('/api/v1/', (req, res) => {
-  res.json({ message: "hello world"})
-})
+// app.get('/api/v1/', (req, res) => {
+//   res.json({ message: "hello world"})
+// })
 
 app.listen(port, () => {
   console.log("Server listing port : " + port);
